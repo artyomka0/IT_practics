734636145af080f48614b305e05e0f80ef52bb3e
artyomka0 <qwertyartyom772@gmail.com>
2021-03-31 19:31:13 +0300

    work 12&13

---
+++ IT_Practika_12/IT_Practika_12/IT_Practika_12/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace IT_Practika_12
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+      System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+      System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+      System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+      System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+      System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+      this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.button4 = new System.Windows.Forms.Button();
+      this.button5 = new System.Windows.Forms.Button();
+      this.dataGridView1 = new System.Windows.Forms.DataGridView();
+      this.dataGridView2 = new System.Windows.Forms.DataGridView();
+      this.dataGridView3 = new System.Windows.Forms.DataGridView();
+      this.dataGridView4 = new System.Windows.Forms.DataGridView();
+      this.dataGridView5 = new System.Windows.Forms.DataGridView();
+      ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // chart1
+      // 
+      this.chart1.BorderlineWidth = 3;
+      chartArea2.Name = "ChartArea1";
+      this.chart1.ChartAreas.Add(chartArea2);
+      legend2.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.Left;
+      legend2.Name = "Legend1";
+      this.chart1.Legends.Add(legend2);
+      this.chart1.Location = new System.Drawing.Point(13, 313);
+      this.chart1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.chart1.Name = "chart1";
+      series4.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.Top;
+      series4.BorderWidth = 3;
+      series4.ChartArea = "ChartArea1";
+      series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+      series4.LabelBorderWidth = 3;
+      series4.Legend = "Legend1";
+      series4.Name = "Полином первой степени";
+      series5.BorderWidth = 3;
+      series5.ChartArea = "ChartArea1";
+      series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+      series5.LabelBorderWidth = 3;
+      series5.Legend = "Legend1";
+      series5.Name = "Полином второй степени";
+      series6.BorderWidth = 3;
+      series6.ChartArea = "ChartArea1";
+      series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+      series6.LabelBorderWidth = 3;
+      series6.Legend = "Legend1";
+      series6.Name = "Начальные данные";
+      this.chart1.Series.Add(series4);
+      this.chart1.Series.Add(series5);
+      this.chart1.Series.Add(series6);
+      this.chart1.Size = new System.Drawing.Size(884, 363);
+      this.chart1.TabIndex = 0;
+      this.chart1.Text = "chart1";
+      this.chart1.Click += new System.EventHandler(this.chart1_Click);
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(404, 13);
+      this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(111, 118);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "Решить сис 1";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(404, 154);
+      this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(111, 139);
+      this.button2.TabIndex = 2;
+      this.button2.Text = "Решить сис 2";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // button3
+      // 
+      this.button3.Location = new System.Drawing.Point(622, 418);
+      this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(250, 50);
+      this.button3.TabIndex = 3;
+      this.button3.Text = "Отобразить начальные точки";
+      this.button3.UseVisualStyleBackColor = true;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // button4
+      // 
+      this.button4.Location = new System.Drawing.Point(622, 505);
+      this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.button4.Name = "button4";
+      this.button4.Size = new System.Drawing.Size(250, 44);
+      this.button4.TabIndex = 4;
+      this.button4.Text = "Построить полином 1ой степени";
+      this.button4.UseVisualStyleBackColor = true;
+      this.button4.Click += new System.EventHandler(this.button4_Click);
+      // 
+      // button5
+      // 
+      this.button5.Location = new System.Drawing.Point(622, 577);
+      this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.button5.Name = "button5";
+      this.button5.Size = new System.Drawing.Size(250, 40);
+      this.button5.TabIndex = 5;
+      this.button5.Text = "Построить полином 2ой степени";
+      this.button5.UseVisualStyleBackColor = true;
+      this.button5.Click += new System.EventHandler(this.button5_Click);
+      // 
+      // dataGridView1
+      // 
+      this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView1.Location = new System.Drawing.Point(918, 313);
+      this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.dataGridView1.Name = "dataGridView1";
+      this.dataGridView1.RowHeadersWidth = 51;
+      this.dataGridView1.Size = new System.Drawing.Size(363, 363);
+      this.dataGridView1.TabIndex = 6;
+      this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+      // 
+      // dataGridView2
+      // 
+      this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView2.Location = new System.Drawing.Point(541, 15);
+      this.dataGridView2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.dataGridView2.Name = "dataGridView2";
+      this.dataGridView2.RowHeadersWidth = 51;
+      this.dataGridView2.Size = new System.Drawing.Size(740, 116);
+      this.dataGridView2.TabIndex = 7;
+      this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+      // 
+      // dataGridView3
+      // 
+      this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView3.Location = new System.Drawing.Point(13, 13);
+      this.dataGridView3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.dataGridView3.Name = "dataGridView3";
+      this.dataGridView3.RowHeadersWidth = 51;
+      this.dataGridView3.Size = new System.Drawing.Size(368, 118);
+      this.dataGridView3.TabIndex = 8;
+      this.dataGridView3.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView3_CellContentClick);
+      // 
+      // dataGridView4
+      // 
+      this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView4.Location = new System.Drawing.Point(541, 154);
+      this.dataGridView4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.dataGridView4.Name = "dataGridView4";
+      this.dataGridView4.RowHeadersWidth = 51;
+      this.dataGridView4.Size = new System.Drawing.Size(740, 139);
+      this.dataGridView4.TabIndex = 9;
+      this.dataGridView4.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView4_CellContentClick);
+      // 
+      // dataGridView5
+      // 
+      this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView5.Location = new System.Drawing.Point(13, 154);
+      this.dataGridView5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.dataGridView5.Name = "dataGridView5";
+      this.dataGridView5.RowHeadersWidth = 51;
+      this.dataGridView5.Size = new System.Drawing.Size(368, 139);
+      this.dataGridView5.TabIndex = 10;
+      this.dataGridView5.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView5_CellContentClick);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1312, 690);
+      this.Controls.Add(this.dataGridView5);
+      this.Controls.Add(this.dataGridView4);
+      this.Controls.Add(this.dataGridView3);
+      this.Controls.Add(this.dataGridView2);
+      this.Controls.Add(this.dataGridView1);
+      this.Controls.Add(this.button5);
+      this.Controls.Add(this.button4);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.chart1);
+      this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+      this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridView dataGridView2;
+		private System.Windows.Forms.DataGridView dataGridView3;
+		private System.Windows.Forms.DataGridView dataGridView4;
+		private System.Windows.Forms.DataGridView dataGridView5;
+	}
+}
+
---
+++ IT_Practika_12/IT_Practika_12/IT_Practika_12/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IT_Practika_12
+{
+	public partial class Form1 : Form  //Вариант 8
+	{
+		double[] x = {5,5.2,5.4,5.6,5.8,6};
+		double[] y = {3,2,5,2,2,3};
+		
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		public double S(int count) 
+		{
+			double s=0;
+			for (int i = 0; i < 6; i++) s += Math.Pow(x[i], count);
+			return s;
+		}
+		public double b(int count)
+		{
+			double s = 0;
+			for (int i = 0; i < 6; i++) s += y[i]*Math.Pow(x[i], count);
+			return s;
+		}
+		public double C(int count)
+		{
+			double a = 0;
+			if ((count == 3) || (count == 6))
+			{
+				a = b(count / 3) - (b(0) * S(2 + count / 3)) / S(2);
+			}
+			if ((count == 1) || (count == 2))
+			{
+				a = S(count) - (S(count-1) * S(3)) / S(2);
+			}
+			if ((count == 4) || (count == 5))
+			{
+				a = S(count-2) - (S(count - 4) * S(4)) / S(2);
+			}
+			return a;
+		}
+		public double polin1_A0()
+		{
+			double a = (b(1)*S(1)-b(0)*S(2))/(S(1)*S(1)-S(2)*S(0));
+			return a;
+		}
+		public double polin1_A1()
+		{
+			double a = (b(0) - S(0) * polin1_A0()) / S(1);
+			return a;
+		}
+		
+		public double polin2_A0()
+		{
+			double a = (C(6)*C(2)-C(5)*C(3))/(C(2)*C(4)-C(5)*C(1));
+			return a;
+		}
+		public double polin2_A1()
+		{
+			double a = (C(3)-C(1)*polin2_A0())/C(2);
+			return a;
+		}
+		public double polin2_A2()
+		{
+			double a =(b(0)-S(1)*polin2_A1()-S(0)*polin2_A0())/S(2);
+			return a;
+		}
+		private void chart1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e) // Полин 1
+		{
+			dataGridView2.RowCount = 2; //Указываем количество строк	
+			dataGridView2.ColumnCount = 3; //Указываем количество столбцов			
+			dataGridView2.Columns[0].HeaderCell.Value = "матрица системы 1";
+
+			for (int i = 0; i < 2; i++)
+				for (int j = 0; j < 3; j++)
+				{
+					if ((j == 0) && (i == 0))
+						dataGridView2.Rows[i].Cells[j].Value = Convert.ToString(S(0));
+					if ((j == 1) && (i == 0))
+						dataGridView2.Rows[i].Cells[j].Value = Convert.ToString(S(1));
+					if ((j == 2) && (i == 0))
+						dataGridView2.Rows[i].Cells[j].Value = Convert.ToString(b(0));
+					if ((j == 0) && (i == 1))
+						dataGridView2.Rows[i].Cells[j].Value = Convert.ToString(S(1));
+					if ((j == 1) && (i == 1))
+						dataGridView2.Rows[i].Cells[j].Value = Convert.ToString(S(2));
+					if ((j == 2) && (i == 1))
+						dataGridView2.Rows[i].Cells[j].Value = Convert.ToString(b(1));
+
+				}
+		}
+
+		private void button2_Click(object sender, EventArgs e) // Полин 2
+		{
+			dataGridView4.RowCount = 3; //Указываем количество строк	
+			dataGridView4.ColumnCount = 4; //Указываем количество столбцов	
+			dataGridView4.Columns[0].HeaderCell.Value = "матрица системы 2";
+			for (int i = 0; i < 3; i++)
+				for (int j = 0; j < 4; j++)
+				{					
+						if((i==0)&&	(j!=3))
+						dataGridView4.Rows[i].Cells[j].Value = Convert.ToString(S(j));
+						if ((i == 1) && (j != 3))
+						dataGridView4.Rows[i].Cells[j].Value = Convert.ToString(S(j+1));
+						if ((i == 2) && (j != 3))
+						dataGridView4.Rows[i].Cells[j].Value = Convert.ToString(S(j+2));
+						if (j==3)
+						dataGridView4.Rows[i].Cells[j].Value = Convert.ToString(b(i));
+				}
+		}
+
+		private void button3_Click(object sender, EventArgs e) //Начальные точки
+		{
+			chart1.Series[2].Points.DataBindXY(x, y);
+		}
+
+		private void button4_Click(object sender, EventArgs e) // полин 1 граф
+		{
+			double[] y1=new double[6];
+			for	(int i=0; i < 6; i++)
+			{
+				y1[i] = polin1_A0() + polin1_A1()* x[i];								
+			}
+			chart1.Series[0].Points.DataBindXY(x, y1);					
+		}
+
+		private void button5_Click(object sender, EventArgs e) // полин 2 граф
+		{
+			double[] y1 = new double[6];
+			for (int i = 0; i < 6; i++)
+			{
+				y1[i] = polin2_A0() + polin2_A1() * x[i]+ polin2_A2()* x[i]* x[i];
+			}
+			chart1.Series[1].Points.DataBindXY(x, y1);
+		}
+
+		private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			dataGridView1.RowCount = 6;	
+			dataGridView1.ColumnCount = 2;
+			dataGridView1.Columns[0].HeaderCell.Value = "х";
+			dataGridView1.Columns[1].HeaderCell.Value = "у";
+			for (int i = 0; i < 6; i++)
+				for (int j = 0; j < 2; j++)
+				{
+					if (j == 0)
+						dataGridView1.Rows[i].Cells[j].Value = Convert.ToString(x[i]);
+					else
+						dataGridView1.Rows[i].Cells[j].Value = Convert.ToString(y[i]);
+				}
+			dataGridView3.RowCount = 2;
+			dataGridView3.ColumnCount = 2;
+			dataGridView3.Columns[0].HeaderCell.Value = "коэффицент";
+			dataGridView3.Columns[1].HeaderCell.Value = "значение";
+
+			for (int i = 0; i < 2; i++)
+				for (int j = 0; j < 2; j++)
+				{
+					if ((j == 0) && (i == 0))
+						dataGridView3.Rows[i].Cells[j].Value = "a0:";
+					if ((j == 1) && (i == 0))
+						dataGridView3.Rows[i].Cells[j].Value = Convert.ToString(polin1_A0());
+					if ((j == 0) && (i == 1))
+						dataGridView3.Rows[i].Cells[j].Value = "a1:";
+					if ((j == 1) && (i == 1))
+						dataGridView3.Rows[i].Cells[j].Value = Convert.ToString(polin1_A1());
+					
+				}
+			dataGridView5.RowCount = 3;
+			dataGridView5.ColumnCount = 2;
+			dataGridView5.Columns[0].HeaderCell.Value = "коэффицент";
+			dataGridView5.Columns[1].HeaderCell.Value = "значение";
+			for (int i = 0; i < 3; i++)
+				for (int j = 0; j < 2; j++)
+				{
+					if ((j == 0) && (i == 0))	
+						dataGridView5.Rows[i].Cells[j].Value = "a0";
+					if ((j == 1) && (i == 0))
+						dataGridView5.Rows[i].Cells[j].Value = Convert.ToString(polin2_A0());
+					if ((j == 0) && (i == 1))
+						dataGridView5.Rows[i].Cells[j].Value = "a1";
+					if ((j == 1) && (i == 1))
+						dataGridView5.Rows[i].Cells[j].Value = Convert.ToString(polin2_A1());
+					if ((j == 0) && (i == 2))
+						dataGridView5.Rows[i].Cells[j].Value = "a2";
+					if ((j == 1) && (i == 2))
+						dataGridView5.Rows[i].Cells[j].Value = Convert.ToString(polin2_A2());
+				}
+		}
+
+		private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+			
+		}
+
+		private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+
+		}
+
+		private void dataGridView5_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+
+		}
+
+		private void dataGridView4_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+
+		}
+	}
+}
